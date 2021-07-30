fb93cc4bc4dc96b5d58f06c716c072647eae6ab4
Nikhil <dev.nikhil@hotmail.com>
2021-07-31 00:18:42 +0530

    env added in tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,7 +7,7 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 
 dotenv.config();
 const start = async () => {
-  console.log("Starting....");
+  console.log("Starting...., env added");
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
   if (!process.env.NATS_CLUSTER_ID)
