1a53a93b0a82d7402f38dcc78548e4899a595a2a
Nikhil <dev.nikhil@hotmail.com>
2021-08-12 15:13:57 +0530

    big fixes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "http://www.ticketing.devnikhil.com/",
+      baseURL: "www.ticketing.devnikhil.com",
       headers: req.headers,
     });
   } else {
